94f93423136385e9c0904e4136f5bf8f9992dff3
Dario Schor <1176025+dschor5@users.noreply.github.com>
2021-07-14 23:07:35 -0400

    Changed login to modal.

--- modules/home.php
+++ modules/home.php
@@ -9,6 +9,8 @@ class HomeModule extends DefaultModule
     {
         $content = '';
 
+        $this->addCss('login');
+
         $subaction = (isset($_GET['subaction'])) ? $_GET['subaction'] : 'home';
 
         switch($subaction)
@@ -21,6 +23,43 @@ class HomeModule extends DefaultModule
          return Main::loadTemplate('modules/home.txt', array(), array());
 
     }
+
+    private function login() : string
+    {
+        global $config;
+        $message = '';
+
+        if(isset($_POST['user']) && isset($_POST['pass']))
+        {
+            $usersDao = UsersDao::getInstance();
+            $user = $usersDao->getByUsername($_POST['user']);
+
+            if($user != null && $user->isValidPassword($_POST['pass']))
+            {
+                $this->user = $user;
+                $sessionId = $user->createNewSession();
+                $this->main->setCookie(array('sessionId'=>$sessionId,'username'=>$_POST['user']));
+                header('Location: http://'.$config['site_url'].'/chat');
+            }
+            else
+            {
+            
+                $message = 'ERROR';
+            }
+        }
+
+        return $this->main->loadTemplate('modules/login-login.txt', array('/%message%/'=>$message));
+    }
+
+    private function logout() : string
+    {
+        global $config;
+
+        $this->user = null;
+        $this->main->setCookie(array('sessionId'=>null,'username'=>null));
+        header('Location: http://'.$config['site_url']);
+        return 'Logging out, please wait while you are redirected to the homepage.';
+    }    
 }
 
 ?>
