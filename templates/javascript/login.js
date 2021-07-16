e8bc19d2039ea1b2e74d52679dfeca4990b1c5b4
Dario Schor <schor@ieee.org>
2021-07-16 06:44:22 -0400

    Modified homepage.

--- templates/javascript/login.js
+++ templates/javascript/login.js
@@ -1,9 +1,39 @@
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById('loginform');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
+
+$(document).ready(function() {
+    $('#butlogin').on('click', function() {
+		var username = $('#uname').val();
+		var password = $('#upass').val();
+		if(username != '' && password != '') {
+			$.ajax({
+				url:  base_url.concat('/login'),
+				type: "POST",
+				data: {
+					uname: username,
+					upass: password						
+				},
+				statusCode: {
+                    200: function() {
+                        location.href = base_url.concat('/chat');
+                    },
+                    201: function() {
+                        $('#response').text('Invalid username or password.');
+                        $('#response').show();
+                    }
+                }
+			});
+		}
+		else{
+			$('#response').text('Invalid username or password.');
+            $('#response').show();
+		}
+	});
+});
