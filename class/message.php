2690033bcc88f0ba02f9cf3b836d7f51b00d179d
Dario Schor <dario.schor@und.edu>
2021-08-19 09:28:53 -0400

    Typo

--- class/message.php
+++ class/message.php
@@ -18,7 +18,7 @@ class Message
         {
             $this->file = new FileUpload(
                 array_intersect_key($this->data, 
-                array('message_id'=>0, 'server_name'=>0, 'original_name'=>0, 'mime_type'=>0)))
+                    array('message_id'=>0, 'server_name'=>0, 'original_name'=>0, 'mime_type'=>0))
             );
         }
     }
