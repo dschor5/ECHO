2c939204ea228cffe688b73f7a4f37f3e447ec38
Dario Schor <1176025+dschor5@users.noreply.github.com>
2022-04-24 02:02:21 -0500

    rd

--- modules/file.php
+++ modules/file.php
@@ -40,7 +40,6 @@ class FileModule implements Module
         {
             $messageFileDao = MessageFileDao::getInstance();
             $file = $messageFileDao->getFile($fileId, $this->user->user_id);
-            Logger::warning('Get '.$fileId.' == '.$file);
         }
 
         // Also catches the case where the user does not have 
