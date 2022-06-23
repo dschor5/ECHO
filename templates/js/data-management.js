09bf4b51553dfa239a72902465d532fb0365f1f8
Dario Schor <1176025+dschor5@users.noreply.github.com>
2022-06-23 11:51:45 -0500

    Variable name error

--- templates/js/data-management.js
+++ templates/js/data-management.js
@@ -50,7 +50,7 @@ function confirmAction(subaction, id, str) {
             $('.modal-confirm-body').text("Are you sure you want to delete the " + str + "?");
             $('#confirm-btn').text('Delete Archive');
         }
-        else if(subacton == 'resetlog') {
+        else if(subaction == 'resetlog') {
             $('#dialog-confirm').dialog({title: 'Reset System Log'});
             $('.modal-confirm-body').text("Are you sure you want to reset the System Log?");
             $('#confirm-btn').text('Reset System Log');
